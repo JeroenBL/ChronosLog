c0d542c88c18ce00c1e5fe736fb180d79ec661b4
jblaauw <j.blaauw@tools4ever.com>
2021-09-16 17:16:33 +0200

    first draft
    first draft  / outline of the project

---
+++ src/Cmdlets/Write-SerilogMessage.cs
@@ -0,0 +1,28 @@
+﻿using ChronosLog.Services;
+using Serilog.Events;
+using System.Management.Automation;
+
+namespace ChronosLog.Cmdlets
+{
+    [Cmdlet(VerbsCommunications.Write, "SeriLogMessage")]
+    public class Write_SerilogMessage : PSCmdlet
+    {
+        private SeriLogInitializationService _seriLogInitializationService;
+
+        public Write_SerilogMessage()
+        {
+            _seriLogInitializationService = new SeriLogInitializationService();
+        }
+
+        [Parameter(Mandatory = true)]
+        public string Message { get; set; }
+
+        [Parameter(Mandatory = true)]
+        public LogEventLevel LogLevel { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            _seriLogInitializationService.WriteMessage(LogLevel, Message);
+        }
+    }
+}
---
+++ src/Services/SeriLogInitializationService.cs
@@ -0,0 +1,48 @@
+﻿using Serilog;
+using Serilog.Events;
+
+namespace ChronosLog.Services
+{
+    public class SeriLogInitializationService
+    {
+        public SeriLogInitializationService()
+        {
+            InitializeLogger();
+        }
+
+        public void InitializeLogger()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .MinimumLevel.Verbose()
+                .CreateLogger();
+        }
+
+        public void WriteMessage(LogEventLevel logLevel, string message)
+        {
+            switch (logLevel)
+            {
+                case LogEventLevel.Verbose:
+                    Log.Verbose(message);
+                    break;
+                case LogEventLevel.Debug:
+                    Log.Debug(message);
+                    break;
+                case LogEventLevel.Information:
+                    Log.Information(message);
+                    break;
+                case LogEventLevel.Warning:
+                    Log.Warning(message);
+                    break;
+                case LogEventLevel.Error:
+                    Log.Error(message);
+                    break;
+                case LogEventLevel.Fatal:
+                    Log.Fatal(message);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
